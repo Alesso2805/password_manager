0158a7b2f3a912affed3a15e11988bf4afa40225
alesso2805 <alessandrodp2222@gmail.com>
2025-03-16 11:29:34 -0500

    incorporated password copying to clipboard, and saving data to a txt file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # logic to generate random passsword
@@ -8,24 +8,26 @@ def generate_password():
     import random
     import string
     password_entry.delete(0, END)
-    password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=16))
+    password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=8))
     password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# logic to save passwords
+# logic to save passwords in data.txt
 def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    if len(website) == 0 or len(email) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\nPassword: {password}\nIs it ok to save?")
         if is_ok:
-            passwords.insert(END, f"{website} | {email} | {password}")
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -46,6 +48,7 @@ email_label = Label(text="Email/Username:", padx=5, pady=5)
 email_label.grid(row=2, column=0)
 email_entry = Entry(width=39)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "alessandrodp2222@gmail.com")
 password_label = Label(text="Password:", padx=5, pady=5)
 password_label.grid(row=3, column=0)
 password_entry = Entry(width=21)
@@ -54,16 +57,6 @@ password_button = Button(text="Generate Password")
 password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=33)
 add_button.grid(row=4, column=1, columnspan=2)
-
-# List of saved passwords
-passwords_label = Label(text="Saved Passwords")
-passwords_label.grid(row=5, column=1)
-passwords = Listbox(width=45)
-passwords.grid(row=6, column=1, columnspan=2)
-scrollbar = Scrollbar()
-scrollbar.grid(row=6, column=3)
-passwords.config(yscrollcommand=scrollbar.set)
-scrollbar.config(command=passwords.yview)
 
 add_button.config(command=save_password)
 
